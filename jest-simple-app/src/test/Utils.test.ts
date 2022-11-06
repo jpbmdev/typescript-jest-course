546fc64f96ab0b371b9acf0ec10ad686eeb0c1db
jpbmdev <jpbmdev@gmail.com>
2022-11-06 15:04:01 -0500

    :rocket: jest simple app finished

---
+++ jest-simple-app/src/test/Utils.test.ts
@@ -0,0 +1,42 @@
+import { Utils } from "../app/Utils";
+
+describe("Utils test suite", () => {
+  test("first test", () => {
+    const result = Utils.toUpperCase("abc");
+    expect(result).toBe("ABC");
+  });
+
+  test("parse simple URL", () => {
+    const parsedUrl = Utils.parseUrl("http://localhost:8080/login");
+    expect(parsedUrl.href).toBe("http://localhost:8080/login");
+    expect(parsedUrl.port).toBe("8080");
+    expect(parsedUrl.protocol).toBe("http:");
+    expect(parsedUrl.query).toEqual({});
+  });
+
+  test("parse URL with query", () => {
+    const parsedUrl = Utils.parseUrl(
+      "http://localhost:8080/login?user=user&password=pass"
+    );
+    const expectedQuery = {
+      user: "user",
+      password: "pass",
+    };
+    expect(parsedUrl.query).toEqual(expectedQuery);
+  });
+
+  test("test invalid URL", () => {
+    expect(() => {
+      Utils.parseUrl("");
+    }).toThrowError("Empty url");
+  });
+
+  test("test invalid URL with try catch", () => {
+    try {
+      Utils.parseUrl("");
+    } catch (error) {
+      expect(error).toBeInstanceOf(Error);
+      expect(error).toHaveProperty("message", "Empty url!");
+    }
+  });
+});
