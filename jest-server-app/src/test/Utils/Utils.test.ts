a07428557f55a7bb76749e2675d1499c4247045f
jpbmdev <jpbmdev@gmail.com>
2022-11-08 15:41:47 -0500

    :rocket: tests finished

--- jest-server-app/src/test/Utils/Utils.test.ts
+++ jest-server-app/src/test/Utils/Utils.test.ts
@@ -1,34 +1,29 @@
-import { IncomingMessage } from "http";
-import { Utils } from "../../app/Utils/Utils";
+import { Utils } from '../../app/Utils/Utils'
+import { IncomingMessage } from 'http';
 
-describe("Utils test suite", () => {
-  test("getRequestPath valid request", () => {
-    const request = {
-      url: "http://localhost:8080/login",
-    } as IncomingMessage;
+describe('Utils test suite', () => {
 
-    const resultPath = Utils.getRequestBasePath(request);
+    test('getRequestPath valid request', () => {
+        const request = {
+            url: 'http://localhost:8080/login'
+        } as IncomingMessage;
+        const resultPath = Utils.getRequestBasePath(request);
+        expect(resultPath).toBe('login');
+    });
 
-    expect(resultPath).toBe("login");
-  });
+    test('getRequestPath with no path name', () => {
+        const request = {
+            url: 'http://localhost:8080/'
+        } as IncomingMessage;
+        const resultPath = Utils.getRequestBasePath(request);
+        expect(resultPath).toBeFalsy();
+    });
 
-  test("getRequestPath with no path name", () => {
-    const request = {
-      url: "http://localhost:8080/",
-    } as IncomingMessage;
-
-    const resultPath = Utils.getRequestBasePath(request);
-
-    expect(resultPath).toBeFalsy();
-  });
-
-  test("getRequestPath with invalid url", () => {
-    const request = {
-      url: "",
-    } as IncomingMessage;
-
-    const resultPath = Utils.getRequestBasePath(request);
-
-    expect(resultPath).toBeFalsy();
-  });
+    test('getRequestPath with no path name', () => {
+        const request = {
+            url: ''
+        } as IncomingMessage;
+        const resultPath = Utils.getRequestBasePath(request);
+        expect(resultPath).toBeFalsy();
+    });
 });
